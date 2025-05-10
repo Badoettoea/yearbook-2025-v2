a3db469cfdc727946c4829ae028c5efcdd316dc4
badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-05-10 10:45:31 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite';
+// File: vite.config.js
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  define: {
-    'import.meta.env': process.env,
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
-});
+  server: {
+    port: 3000
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        profile: 'profile.html',
+        gallery: 'gallery.html',
+        memories: 'memories.html'
+      }
+    }
+  }
+})
