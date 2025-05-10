2caa9fe0a6db8cad98506dc8cb345c9fa27a0807
Badoettoea <minimatis25@gmail.com>
2025-05-10 18:58:23 +0700

    Add JS and Vite config

--- main.js
+++
@@ -1,6 +0,0 @@
-import { supabase } from './supabase.js';
-
-document.addEventListener('DOMContentLoaded', async () => {
-  console.log('Supabase initialized:', supabase);
-  // Tambahkan logika lainnya di sini
-});
