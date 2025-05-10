c7fbb8e8ff278b553617cd8089ac5705f42b44c4
badoettoea <87426546+Badoettoea@users.noreply.github.com>
2025-05-10 11:38:22 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // File: vite.config.js
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // ✅ Tambahkan ini
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  plugins: [react()], // ✅ Tambahkan ini agar Vite tahu kamu pakai JSX
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
